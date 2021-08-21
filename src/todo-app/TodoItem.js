aed76d765598d98a7708a3552f876fe122310224
orion14th <orion14th@gmail.com>
2021-08-21 11:42:51 -0600

    Update TodoItem.js

--- src/todo-app/TodoItem.js
+++ src/todo-app/TodoItem.js
@@ -1,1 +1,27 @@
 
+import React from "react"
+
+function TodoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+
+    return (
+        <div 
+            className="todo-item"
+            onClick={() => props.handleChange(props.todo.id)}
+        >
+            <input 
+                onChange={() => {}} 
+                name="completed" 
+                type="checkbox" 
+                checked={props.todo.completed}
+            />
+            <p style={props.todo.completed ? completedStyle : null}>{props.todo.text}</p>
+        </div>
+    )    
+}
+
+export default TodoItem
