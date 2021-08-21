2ac729425856dab466db1456e664aadde290271d
orion14th <orion14th@gmail.com>
2021-08-21 11:43:08 -0600

    Update TodoList.js

--- src/todo-app/TodoList.js
+++ src/todo-app/TodoList.js
@@ -1,1 +1,47 @@
+import React, {Component} from "react"
+import todosData from "./todosData.js"
+import TodoItem from "./TodoItem"
 
+class TodoList extends Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodosList = prevState.todos.map(todo => {
+                if(todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodosList
+            }
+        })
+    }
+    
+    render() {
+        const todoComponents = this.state.todos.map(todo => {
+            return (
+                <TodoItem
+                    todo={todo} 
+                    key={todo.id}
+                    handleChange={this.handleChange}
+                />
+            )
+        })
+
+        return (
+            <div className="todo-list">
+                {todoComponents}
+            </div>
+        )
+    }
+}
+
+export default TodoList
