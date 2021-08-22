94e589725dfd969a6ed6edb8badfd81a280c3571
orion14th <orion14th@gmail.com>
2021-08-21 18:53:02 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import MemeApp from './meme-generator/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(<MemeApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
