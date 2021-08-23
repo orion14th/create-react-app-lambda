1ffecc3f54a47e012c1355759483d2fc5fa849d2
orion14th <orion14th@gmail.com>
2021-08-23 10:35:19 -0600

    Update App.js

--- src/josh-test/App.js
+++ src/josh-test/App.js
@@ -1,10 +1,14 @@
 import React from "react"
 
 function JoshTest(){
+  const JoshCSS={
   
+  fontSize:"3em",
+    color:"#666"
+  }
   return(
   
-  <h1> Test </h1>
+  <h1 className={JoshCSS}> Test </h1>
     
   )
   
