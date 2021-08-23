92ecfdb4f8e4fe2c7d156a9a7a39e1f2d66833c1
orion14th <orion14th@gmail.com>
2021-08-23 10:44:07 -0600

    Update Header.js

--- src/josh-test/Header.js
+++ src/josh-test/Header.js
@@ -1,1 +1,23 @@
+import React from "react"
 
+function Header(){
+  const h2style={
+  
+    fontSize:"1em",
+    color:"#333"
+  
+  }
+  
+  return(
+    
+    <h2 style={h2style}>Try this now h2</h2>
+    )
+  
+}
+
+
+
+
+
+
+export default Header
