5d734287f94831c2d2ee5ea7931a14569b94437d
orion14th <orion14th@gmail.com>
2021-08-23 10:50:00 -0600

    Update App.js

--- src/josh-test/App.js
+++ src/josh-test/App.js
@@ -2,11 +2,7 @@ import React from "react"
 import Header from "Header"
 import Footer from "Footer"
 
-function JoshTest(){
-  const JoshCSS={
-    fontStyle: "italic",
-        color: "#000000"
-  }
+ 
   return(<div>
   <Header />
  
