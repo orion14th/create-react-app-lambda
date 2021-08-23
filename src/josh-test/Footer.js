451d4e86d60cd62a494fd559035e4d2cad85cc11
orion14th <orion14th@gmail.com>
2021-08-23 10:44:37 -0600

    Update Footer.js

--- src/josh-test/Footer.js
+++ src/josh-test/Footer.js
@@ -1,1 +1,24 @@
 
+import React from "react"
+
+function Footer(){
+  const h3style={
+  
+    fontSize:"2em",
+    color:"#000"
+  
+  }
+  
+  return(
+    
+    <h2 style={h3style}>Try this now h2</h2>
+    )
+  
+}
+
+
+
+
+
+
+export default Footer
