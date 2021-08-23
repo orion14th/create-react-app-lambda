c671f838d4657bb1cf78c47ce7ff7df55f06720d
orion14th <orion14th@gmail.com>
2021-08-23 10:55:01 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 //import TodoApp from './todo-app/App';
-//import MemeApp from './meme-generator/App';
-import JoshTest from './josh-test/App';
+import MemeApp from './meme-generator/App';
+//import JoshTest from './josh-test/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<JoshTest />, document.getElementById('root'));
+ReactDOM.render(<MemeApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
