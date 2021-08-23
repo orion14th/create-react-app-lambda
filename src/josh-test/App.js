3acee0fc50dadd79fd92800957917e922804fb5b
orion14th <orion14th@gmail.com>
2021-08-23 10:46:24 -0600

    Update App.js

--- src/josh-test/App.js
+++ src/josh-test/App.js
@@ -3,8 +3,7 @@ import React from "react"
 function JoshTest(){
   const JoshCSS={
     fontStyle: "italic",
-        color: "#000000",
-        textDecoration: "line-through"
+        color: "#000000"
   }
   return(
   <Header />
