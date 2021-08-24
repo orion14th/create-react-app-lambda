3e8d593fac2a347bfc1e305785b7b76a92493d65
orion14th <orion14th@gmail.com>
2021-08-24 05:56:35 -0600

    Update App.js

--- src/apis/App.js
+++ src/apis/App.js
@@ -1,1 +1,23 @@
+import React, {Component} from "react"
 
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            character: {}
+        }
+    }
+    
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("https://swapi.co/api/people/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    character: data
+                })
+            })
+    }
