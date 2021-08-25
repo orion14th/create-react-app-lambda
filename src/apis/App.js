b06f7912b5dd0eefbb4917136fc1c771f0844944
orion14th <orion14th@gmail.com>
2021-08-25 10:37:30 -0600

    Update App.js

--- src/apis/App.js
+++ src/apis/App.js
@@ -1,39 +1,44 @@
 import React, {Component} from "react"
 
-class App extends Component{
-    constructor(){
+class App extends Component {
+    constructor() {
         super()
-    this.state={
-    
-    character:{}
-    
+        this.state = {
+            firstName: "",
+            lastName: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
     }
-   | 
     
-}
-
-ComponentDidMount(){
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
     
-    fetch("https://swapi.dev/api/people/1")
-    .then(response => response.json())
-    .then(data => {
-    this.setState({
-    
-               character:data
-    
-    })
- 
-    
-    })
-    
-}
-
-render(){
-    
-    return(
-    
-    <div> {this.state.character.name} </div>
-    )
+    render() {
+        return (
+            <form>
+                <input 
+                    type="text" 
+                    value={this.state.firstName} 
+                    name="firstName" 
+                    placeholder="First Name" 
+                    onChange={this.handleChange} 
+                />
+                <br />
+                <input 
+                    type="text" 
+                    value={this.state.lastName} 
+                    name="lastName" 
+                    placeholder="Last Name" 
+                    onChange={this.handleChange} 
+                />
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </form>
+        )
+    }
 }
 
 export default App
