37c0cd7812bf9e53c6e818e2c34d319da1e68f0a
orion14th <orion14th@gmail.com>
2021-08-25 10:50:11 -0600

    Update App.js

--- src/apis/App.js
+++ src/apis/App.js
@@ -1,44 +1,45 @@
 import React, {Component} from "react"
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            firstName: "",
-            lastName: ""
+class App extends Component(){
+    constructor(){
+        
+     super()
+        
+        this.state={
+        
+         firstname:""   
+        
         }
-        this.handleChange = this.handleChange.bind(this)
+        
+        
     }
     
-    handleChange(event) {
-        const {name, value} = event.target
-        this.setState({
-            [name]: value
-        })
-    }
     
-    render() {
-        return (
-            <form>
-                <input 
-                    type="text" 
-                    value={this.state.firstName} 
-                    name="firstName" 
-                    placeholder="First Name" 
-                    onChange={this.handleChange} 
-                />
-                <br />
-                <input 
-                    type="text" 
-                    value={this.state.lastName} 
-                    name="lastName" 
-                    placeholder="Last Name" 
-                    onChange={this.handleChange} 
-                />
-                <h1>{this.state.firstName} {this.state.lastName}</h1>
-            </form>
-        )
-    }
 }
+
+handlerChange(event){
+    
+    this.setState({
+    
+        firstname:event.target.value
+    
+    })
+}
+
+render(){
+    
+ 
+    return(
+    
+    <div> <form> <input name="firstname" type="text" onChange={this.handlerChange} placeholder="firstname"  />  </form>
+
+<p>{this.state.firstname}</p>
+
+</div>
+    
+    )
+    
+}
+
 
 export default App
