24f2c0b2c2d439aa553ea3bbbaa2451b714ec5f2
orion14th <orion14th@gmail.com>
2021-08-25 10:27:41 -0600

    Update App.js

--- src/apis/App.js
+++ src/apis/App.js
@@ -1,38 +1,40 @@
-import React, {Component} from "react"
+import React,{Component} from "react"
 
-// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
-// https://swapi.co/
-// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+class App extends Component{
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            character: {}
-        }
+    this.state={
+    
+        character:{}
+    
     }
     
-    componentDidMount() {
-        fetch("https://swapi.dev/api/people/1")
-            .then(response => response.json())
-            .then(data => this.setState({
-        
-       character:data
-        
-        }) 
-                 
-                 
-                 
-                 )
-    }
-    
-    render() {
-        return (
-            <div>
-            {this.state.character.name}
-            </div>
-        )
-    }
+ 
 }
 
+ComponentDidMount(){
+    
+    fetch("https://swapi.dev/api/people/1")
+    .then(response => response.json())
+     .then(data => {
+        this.setState({
+    
+    character:data
+    
+    })
+        
+    })
+}
+
+render(){
+    
+ 
+    return(
+    
+    <div>   {this.state.character.name}        </div>
+    
+    )
+    
+}
+
+ 
 export default App
